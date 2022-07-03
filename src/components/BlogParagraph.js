8fdf345fd6401ea8b616502125e0cb4d187b3d13
williamjparra <w3webservices.net@gmail.com>
2022-07-03 17:59:01 -0400

    console.log() removed on unnecesary places

--- src/components/BlogParagraph.js
+++ src/components/BlogParagraph.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function BlogParagraph(src) {
-    console.log(src)
   return (
     <div>{src.children.map((e) => {
         console.log("elemens", e)
--- src/components/Image.js
+++ src/components/Image.js
@@ -2,8 +2,6 @@ import React from 'react'
 //import Image from 'gatsby-image'
 
 export default function ImageComponent({src, alt}) {
-  console.log("data", src, alt)
-
   return (
     <img
         src={src}
