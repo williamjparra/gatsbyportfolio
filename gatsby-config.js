613689a1e39b7d66a677b41b8787c9ccd50ded84
williamjparra <w3webservices.net@gmail.com>
2022-10-14 19:31:55 -0400

    gatsby config icons added

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,38 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/bhhulogo.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `src/images/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicon-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicon-96x96.png`,
+            sizes: `96x96`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicon-96x96.png`,
+            sizes: `96x96`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/android-icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/apple-icon-180x180.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+        ] // This path is relative to the root of the site.
       },
     },
     {
