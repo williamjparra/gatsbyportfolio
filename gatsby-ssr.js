6ac2027fff852ff607c61958686089d6e00621b2
williamjparra <w3webservices.net@gmail.com>
2022-10-14 18:40:23 -0400

    gatsby ssr added

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react");
+const Layout = require("./src/containers/layout/index").default
+const { BlogProvider } = require('./src/Context')
+
+exports.wrapRootElement= ({element}) => (
+    <BlogProvider>
+        <Layout>
+            { element }
+        </Layout>
+    </BlogProvider>
+)
